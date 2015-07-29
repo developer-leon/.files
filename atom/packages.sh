ca304972103ef77d4e8e32c6a338cd076e6097da
Jesus Leon <git@jes.mx>
2015-07-29 18:03:55 +0200

    Create packages.sh

---
+++ atom/packages.sh
@@ -0,0 +1,17 @@
+# Install linter
+apm install lint
+
+# Client-side linters
+apm install linter-csslint linter-htmlhint linter-jshin linter-jsonlintt
+
+# Markup linters
+apm install linter-js-yaml linter-xmllint
+
+# Php linters
+apm install linter-php linter-phpcs linter-phpmd
+
+# Helpers
+apm install project-manager advanced-new-file #tool-bar-main
+
+# Theming
+apm install file-icons atom-material-syntax atom-material-ui
